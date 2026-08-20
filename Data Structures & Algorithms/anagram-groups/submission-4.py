c8ba99c64a75b4bab5a1167842be3f0a68fa62fe
Shravan Balasubramanian <shravanayyappa@gmail.com>
2026-08-20 12:02:33 +0200

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hmap = {}
+
+        for st in strs:
+            sorted_st = "".join(sorted(st))
+
+            if sorted_st in hmap:
+                hmap[sorted_st].append(st)
+            else:
+                hmap[sorted_st] = [st]
+
+        return list(hmap.values())
+        
